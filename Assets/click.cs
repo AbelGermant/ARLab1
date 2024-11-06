10c036f528d43a72e3db19e1885266c0af0bde92
Abel <abel.germant@gmail.com>
2024-11-06 08:02:26 +0100

    Part 3: Interaction with the Virtual Object

---
+++ Assets/click.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class click : MonoBehaviour, IPointerClickHandler
+{
+    public float rotationSpeed = 1000f;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+}
